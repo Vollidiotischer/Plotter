65db016792957cbb21fbc48a11431aa84dcba075
Volt <t.fuechsel@gmx.de>
2021-11-24 18:41:47 +0100

    Fertig

--- Main.cpp
+++ Main.cpp
@@ -11,31 +11,31 @@ struct Point {
     }
 };
 
-void init(); 
-void loop(); 
-void draw(); 
+void init();
+void loop();
+void draw();
 void events();
-double sign(double); 
-double fun(double, double); 
-void drawLine(int x1, int y1, int x2, int y2); 
+short sign(float);
+float fun(float, float);
+void drawLine(int x1, int y1, int x2, int y2);
 
 
-sf::RenderWindow *window;
-sf::RectangleShape *pixel;
+sf::RenderWindow* window;
+sf::RectangleShape* pixel;
 
 int plotSize[2];
-int pixelSize = 2; 
-double widthFactor = 0; 
-double heightFactor = 0; 
-Point *zeroPoint; 
+int pixelSize = 2;
+float widthFactor = 0;
+float heightFactor = 0;
+Point* zeroPoint;
 
 
 
-int main(){
+int main() {
 
-    init(); 
+    init();
 
-    loop(); 
+    loop();
 
     return 0;
 }
@@ -43,35 +43,36 @@ int main(){
 void init() {
     window = new sf::RenderWindow(sf::VideoMode(1000, 1000), "Plotter");
 
-    pixel = new sf::RectangleShape; 
-    (*pixel).setSize({ 1, 1 }); 
-    (*pixel).setFillColor(sf::Color::Black);
+    pixel = new sf::RectangleShape;
 
     plotSize[0] = 20; // X-Axis
     plotSize[1] = 20; // Y-Axis
 
-    pixelSize = 2; // Size of a virtualPixel
+    pixelSize = 5; // Size of a virtualPixel
 
 
-    widthFactor = (double)window->getSize().x / ((double)plotSize[0]);
-    heightFactor = (double)window->getSize().y / ((double)plotSize[1]);
+    widthFactor = (float)window->getSize().x / ((float)plotSize[0]);
+    heightFactor = (float)window->getSize().y / ((float)plotSize[1]);
 
-    zeroPoint = new Point((double)window->getSize().x / 2 + 179, (double)window->getSize().y / 2 - 188); 
+    zeroPoint = new Point((double)window->getSize().x / 2 + 179, (double)window->getSize().y / 2 + 188);
 }
 
 void loop() {
 
     while (window->isOpen()) {
 
-        events(); 
-        draw(); 
+        widthFactor = (float)window->getSize().x / ((float)plotSize[0]);
+        heightFactor = (float)window->getSize().y / ((float)plotSize[1]);
+
+        events();
+        draw();
 
     }
 
 }
 
 void draw() {
-    window->clear(sf::Color::White); 
+    window->clear(sf::Color::White);
 
     // DRAW X AND Y AXIS
 
@@ -102,21 +103,24 @@ void draw() {
 
     // DRAW GRAPH
     pixel->setSize({ (float)pixelSize, (float)pixelSize });
-    pixel->setFillColor(sf::Color::Blue); 
+    pixel->setFillColor(sf::Color::Blue);
 
+    int w = (int)window->getSize().x;
+    int h = (int)window->getSize().y;
 
-    for (int x = 0; x < (int)window->getSize().x; x += 1) { // 
+    for (int x = 0; x < w; x += (pixelSize - 1)) { // 
 
-        for (int y = 0; y < ((int)window->getSize().y); y += 1) {
+        float kx = (x - zeroPoint->x) / widthFactor;
 
-            double kx = (x - zeroPoint->x) / widthFactor;
-            double ky = ((zeroPoint->y - y)) / heightFactor + 1;
+        for (int y = 0; y < h; y += (pixelSize - 1)) {
 
-            double result = sign(fun(ky - 1 / heightFactor, kx - 1 / widthFactor)) + sign(fun(ky - 1 / heightFactor, kx)) + sign(fun(ky, kx - 1 / widthFactor)) + sign(fun(ky, kx)); 
+            float ky = (zeroPoint->y - y) / heightFactor + 1;
+
+            float result = sign(fun(ky - 1 / heightFactor, kx - 1 / widthFactor)) + sign(fun(ky - 1 / heightFactor, kx)) + sign(fun(ky, kx - 1 / widthFactor)) + sign(fun(ky, kx));
 
             if (result > -4 && result < 4) {
 
-                pixel->setPosition(x,  y);
+                pixel->setPosition(x, y);
 
                 window->draw(*pixel);
 
@@ -128,25 +132,52 @@ void draw() {
     }
 
 
-    window->display(); 
+    window->display();
 }
 
 void drawLine(int x1, int y1, int x2, int y2) {
 
-    pixel->setPosition(x1, y1); 
-    pixel->setSize({(float) x2 - x1,(float) y2 - y1 });
-    pixel->setFillColor(sf::Color::Black); 
+    pixel->setPosition(x1, y1);
+    pixel->setSize({ (float)x2 - x1,(float)y2 - y1 });
+    pixel->setFillColor(sf::Color::Black);
 
-    window->draw(*pixel); 
+    window->draw(*pixel);
 
 }
 
 void events() {
+
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+        double x, y;
+        x = sf::Mouse::getPosition(*window).x;
+        y = sf::Mouse::getPosition(*window).y;
+
+        zeroPoint->x = x;
+        zeroPoint->y = y;
+
+
+    }
+
     sf::Event event;
     while (window->pollEvent(event))
     {
         if (event.type == sf::Event::Closed)
             window->close();
+
+        if (event.type == sf::Event::MouseWheelScrolled) {
+            if (event.mouseWheel.x > 0) {
+                if (plotSize[0] > 2) {
+                    plotSize[0] -= 2;
+                }
+                if (plotSize[1] > 2) {
+                    plotSize[1] -= 2;
+                }
+            }
+            else {
+                plotSize[0] += 2;
+                plotSize[1] += 2;
+            }
+        }
 
         if (event.type == sf::Event::KeyPressed) {
             if (event.key.code == sf::Keyboard::Left) {
@@ -162,15 +193,39 @@ void events() {
             if (event.key.code == sf::Keyboard::Down) {
                 zeroPoint->y -= 50;
             }
+
+            if (event.key.code == sf::Keyboard::A) {
+                if (plotSize[0] > 2) {
+                    plotSize[0] -= 2;
+                }
+            }
+            if (event.key.code == sf::Keyboard::W) {
+                if (plotSize[1] > 2) {
+                    plotSize[1] -= 2;
+                }
+            }
+            if (event.key.code == sf::Keyboard::D) {
+                plotSize[0] += 2;
+            }
+            if (event.key.code == sf::Keyboard::S) {
+                plotSize[1] += 2;
+            }
+            if (event.key.code == sf::Keyboard::F) {
+                pixelSize++;
+            }
+            if (event.key.code == sf::Keyboard::R) {
+                if (pixelSize > 2) {
+                    pixelSize--;
+                }
+            }
         }
     }
 }
 
-double sign(double x) {
-    return (x > 1) - (x < 1); 
+short sign(float x) {
+    return (x > 1) - (x < 1);
 }
 
-double fun(double y, double x) {
-    return y - x; 
-
+float fun(float y, float x) {
+    return y - cos(x);
 }
